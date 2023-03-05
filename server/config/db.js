3d418b46b50ed1254c6add99e63e359fd219e6aa
Munish-Bali <78945518+Munish-Bali@users.noreply.github.com>
2023-03-05 11:18:11 -0500

    init

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb://username:PDmbL6McoMnGO1WA@cluster0.i07bgw6.mongodb.net/mydb?retryWrites=true&w=majority"
+};
